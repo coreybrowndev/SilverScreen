0d9f39ca9fcb2eb005d335fa62005fca50c94539
cbpog <bcorey395@gmail.com>
2022-11-14 00:12:32 -0500

    resolve bug related to title check

--- lib/components/movie_parser.dart
+++ lib/components/movie_parser.dart
@@ -3,6 +3,7 @@ import 'movie.dart';
 
 //Instance of movie object
 class MovieParser {
+  final String message = 'loading';
   Movie parseMovie(Map<dynamic, dynamic> jsonData) {
     return Movie(
       genre: List<int>.from(jsonData['genre_ids']),
@@ -10,7 +11,7 @@ class MovieParser {
       isAdultContent: jsonData['adult'] as bool,
       movieId: jsonData['id'] as int,
       movieOverview: jsonData['overview'] as String,
-      movieTitle: jsonData['title'] as String,
+      movieTitle: jsonData['title'],
       releaseDate: jsonData['release_date'] as String,
       posterPath: jsonData['poster_path'] as String,
     );
