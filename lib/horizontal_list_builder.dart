2cd38a578aede50dbb9d420a67667256dbb953d0
cbpog <bcorey395@gmail.com>
2022-11-14 00:10:12 -0500

    update list view and box height

--- lib/horizontal_list_builder.dart
+++ lib/horizontal_list_builder.dart
@@ -1,49 +1,110 @@
-import 'dart:io';
-import 'package:final_project_ss_app/components/movie.dart';
 import 'package:flutter/material.dart';
 
-class DisplayMovieListHorizontally extends StatelessWidget {
-  final List<Movie> listOfMovie;
+class DisplayTrendingMovieListHorizontally extends StatelessWidget {
+  final List trending;
+  final String message = 'Loading';
 
-  const DisplayMovieListHorizontally(
-      {required Key key, required this.listOfMovie})
-      : super(key: key);
+  const DisplayTrendingMovieListHorizontally(
+      {super.key, required this.trending});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 270,
-      child: ListView.builder(
-          itemCount: listOfMovie.length,
-          scrollDirection: Axis.horizontal,
-          itemBuilder: (context, index) {
-            return InkWell(
-              child: SizedBox(
-                child: Column(
-                  children: [
-                    Container(
-                      height: 250,
-                      width: 250,
-                      decoration: BoxDecoration(
-                        image: DecorationImage(
-                            image: NetworkImage(
-                          scale: 0.1,
-                          'https://image.tmdb.org/t/p/w500${listOfMovie[index].posterPath}',
-                        )),
-                      ),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const SizedBox(
+          height: 10,
+        ),
+        SizedBox(
+          height: 270,
+          child: ListView.builder(
+              itemCount: trending.length,
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context, index) {
+                return InkWell(
+                  child: SizedBox(
+                    height: 275,
+                    child: Column(
+                      children: [
+                        Container(
+                          height: 250,
+                          width: 250,
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                                image: NetworkImage(
+                              scale: 0.1,
+                              'https://image.tmdb.org/t/p/w500${trending[index]['poster_path']}',
+                            )),
+                          ),
+                        ),
+                        Text(
+                          trending[index]['title'] ?? message,
+                          style: const TextStyle(
+                            color: Colors.grey,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ],
                     ),
-                    Text(
-                      listOfMovie[index].movieTitle,
-                      style: const TextStyle(
-                        color: Colors.grey,
-                        fontSize: 16,
-                      ),
+                  ),
+                );
+              }),
+        ),
+      ],
+    );
+  }
+}
+
+class DisplayPopularMovieListHorizontally extends StatelessWidget {
+  final List popular;
+  final String message = 'Loading';
+
+  const DisplayPopularMovieListHorizontally({super.key, required this.popular});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const SizedBox(
+          height: 10,
+        ),
+        SizedBox(
+          height: 270,
+          child: ListView.builder(
+              itemCount: popular.length,
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context, index) {
+                return InkWell(
+                  child: SizedBox(
+                    height: 275,
+                    child: Column(
+                      children: [
+                        Container(
+                          height: 250,
+                          width: 250,
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                                image: NetworkImage(
+                              scale: 0.1,
+                              'https://image.tmdb.org/t/p/w500${popular[index]['poster_path']}',
+                            )),
+                          ),
+                        ),
+                        Text(
+                          popular[index]['title'] ?? message,
+                          style: const TextStyle(
+                            color: Colors.grey,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ],
                     ),
-                  ],
-                ),
-              ),
-            );
-          }),
+                  ),
+                );
+              }),
+        ),
+      ],
     );
   }
 }
