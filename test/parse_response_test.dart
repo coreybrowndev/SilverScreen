782fcbd1e6a0b0c2aa689313b6b2ab58b9424a96
Corey <corey.brown@bsu.edu>
2022-11-14 15:47:13 -0500

    Update to access modifier to match correct usage

--- test/parse_response_test.dart
+++ test/parse_response_test.dart
@@ -5,17 +5,17 @@ import 'package:final_project_ss_app/components/movie_parser.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() async {
-  var response = await File('test/api_response.json').readAsString();
-  var popularJson = jsonDecode(response);
-  var movieData = popularJson['results'];
+  final response = await File('test/api_response.json').readAsString();
+  final popularJson = jsonDecode(response);
+  final movieData = popularJson['results'];
   List<Movie> listOfMovies = [];
 
   test('Able to create a movie object from the api response', () {
     for (Map<dynamic, dynamic> movieInfo in movieData) {
-      var parse = MovieParser().parseMovie(movieInfo);
+      final parse = MovieParser().parseMovie(movieInfo);
       listOfMovies.add(parse);
     }
-    var answer = listOfMovies[0].movieTitle;
+    final answer = listOfMovies[0].movieTitle;
 
     expect(answer, 'Black Adam');
   });
