48ec5003dc1d058f285fdc39c42acee3a5da1da7
Noah M <nmmay@bsu.edu>
2022-11-18 16:12:06 -0500

    Remove unused variables in the movie and movie_parser classes.

--- lib/components/movie.dart
+++ lib/components/movie.dart
@@ -2,19 +2,15 @@ import 'dart:core';
 
 class Movie {
   final bool isAdultContent;
-  final int movieId;
   final String movieTitle;
   final String movieOverview;
   final String releaseDate;
   final String posterPath;
-  final bool isSuggested;
   final List<int> genre;
 
   Movie({
     required this.genre,
-    required this.isSuggested,
     required this.isAdultContent,
-    required this.movieId,
     required this.movieOverview,
     required this.movieTitle,
     required this.releaseDate,
--- lib/components/movie_parser.dart
+++ lib/components/movie_parser.dart
@@ -1,19 +1,16 @@
 import 'dart:core';
 import 'movie.dart';
 
-//Instance of movie object
 class MovieParser {
   final String message = 'loading';
   Movie parseMovie(Map<dynamic, dynamic> jsonData) {
     return Movie(
-      genre: List<int>.from(jsonData['genre_ids']),
-      isSuggested: false,
-      isAdultContent: jsonData['adult'] as bool,
-      movieId: jsonData['id'] as int,
-      movieOverview: jsonData['overview'] as String,
-      movieTitle: jsonData['title'],
-      releaseDate: jsonData['release_date'] as String,
-      posterPath: jsonData['poster_path'] as String,
-    );
+        genre: List<int>.from(jsonData['genre_ids']),
+        isAdultContent: jsonData['adult'] as bool,
+        movieOverview: jsonData['overview'] as String,
+        movieTitle: jsonData['title'],
+        releaseDate: jsonData['release_date'] as String,
+        posterPath:
+            'https://image.tmdb.org/t/p/w500${jsonData['poster_path']}');
   }
 }
