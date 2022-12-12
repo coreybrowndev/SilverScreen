a4dc0a2c5b4e87369128dda9d1b1ad7987a2a449
Corey <corey.brown@bsu.edu>
2022-12-12 01:31:59 -0500

    Added popup dialog with instructions for app use

---
+++ lib/movie_constructors/genre_map.dart
@@ -0,0 +1,19 @@
+class GenreMap {
+  Map<int, String> genreMap = {
+    28: 'Action',
+    12: 'Adventure',
+    16: 'Animation',
+    35: 'Comedy',
+    80: 'Crime',
+    18: 'Drama',
+    10751: 'Family',
+    14: 'Fantasy',
+    36: 'History',
+    27: 'Horror',
+    10402: 'Music',
+    9648: 'Mystery',
+    878: 'Science Fiction',
+    53: 'Thriller',
+    10752: 'War',
+  };
+}
--- lib/pages/suggestion_page.dart
+++ lib/pages/suggestion_page.dart
@@ -3,6 +3,7 @@ import 'package:final_project_ss_app/movie_constructors/movie.dart';
 import 'package:final_project_ss_app/network/api_request.dart';
 import 'package:flutter/material.dart';
 import '../movie_card/movie_card.dart';
+import '../movie_constructors/genre_map.dart';
 import '../movie_constructors/user.dart';
 import 'package:final_project_ss_app/movie_lists/suggestions_list.dart';
 
@@ -30,6 +31,7 @@ class _SuggestionPageState extends State<SuggestionPage> {
     setState(() {
       movieList = apiMovieList;
       movieSuggestions = SuggestionList().suggestionGenerator(movieList);
+      showDialog(context: context, builder: _directionsPopupDialog);
     });
   }
 
@@ -56,9 +58,24 @@ class _SuggestionPageState extends State<SuggestionPage> {
     );
   }
 
+  Widget _directionsPopupDialog(BuildContext context) {
+    return AlertDialog(
+      title: const Text('How to navigate through movies'),
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: const <Widget>[
+          Text('Swipe right to like a movie\n'
+              'Swipe left to dislike a movie\n'
+              'Swipe up to add the movie to your watched later list..Enjoy')
+        ],
+      ),
+    );
+  }
+
   List<Widget> _buildButtonWithNames() {
     List<TextButton> buttonList = <TextButton>[];
-    for (var genre in genreMap.entries) {
+    for (var genre in GenreMap().genreMap.entries) {
       buttonList.add(
         TextButton(
           onPressed: () => buildSuggestions(genre.key),
@@ -78,7 +95,7 @@ class _SuggestionPageState extends State<SuggestionPage> {
   Widget genreSelection() => ListView(
       scrollDirection: Axis.horizontal, children: _buildButtonWithNames());
 
-  _swipe(int index, AppinioSwiperDirection direction) {
+  _swipe(int index, AppinioSwiperDirection direction, List<Movie> movieList) {
     if (direction == AppinioSwiperDirection.right) {
       defaultUser.listOfLikedMovie.add(movieList[(index)]);
     } else if (direction == AppinioSwiperDirection.top) {
@@ -88,21 +105,3 @@ class _SuggestionPageState extends State<SuggestionPage> {
     }
   }
 }
-
-Map<int, String> genreMap = {
-  28: 'Action',
-  12: 'Adventure',
-  16: 'Animation',
-  35: 'Comedy',
-  80: 'Crime',
-  18: 'Drama',
-  10751: 'Family',
-  14: 'Fantasy',
-  36: 'History',
-  27: 'Horror',
-  10402: 'Music',
-  9648: 'Mystery',
-  878: 'Science Fiction',
-  53: 'Thriller',
-  10752: 'War',
-};
